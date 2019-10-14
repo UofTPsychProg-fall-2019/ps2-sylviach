f5960ffcf4d9860b8851e1ab9097ba44417a7218
Sylvia C <xmchang@live.com>
2019-10-14 09:16:56 -0400

    Final submission

--- group_analysis_skeleton.py
+++ group_analysis_skeleton.py
@@ -4,6 +4,7 @@
 scene-cat problem set for PSY 1210 - Fall 2018
 
 @author: Michael Mack
+### Assignment completed by Xiao Min Chang
 """
 
 #%% import block 
@@ -20,6 +21,11 @@ import shutil
 #
 testingrooms = ['A','B','C']
 for room in testingrooms:
+    os.chdir('C:/Users/XiaoMin/Documents/GitHub/ps2-sylviach')
+    path="/Users/XiaoMin/Documents/GitHub/ps2-sylviach/"+"testingroom"+ room +"/experiment_data.csv"
+    newpath="/Users/XiaoMin/Documents/GitHub/ps2-sylviach/"+"rawdata/"+"experiemtn_data_"+ room +".csv"
+    shutil.copyfile(path,newpath)
+
 ...
 
 
@@ -29,14 +35,17 @@ for room in testingrooms:
 #
 data = np.empty((0,5))
 for room in testingrooms:
+    newpath="/Users/XiaoMin/Documents/GitHub/ps2-sylviach/"+"rawdata/"+"experiemtn_data_"+ room +".csv"
+    tmp = sp.loadtxt(newpath,delimiter=',')
+    data = np.vstack([data,tmp])
 ...
 
 
 #%%
 # calculate overall average accuracy and average median RT
 #
-acc_avg = ...   # 91.48%
-mrt_avg = ...   # 477.3ms
+acc_avg = np.mean(data[:, 3])   # 91.48%
+mrt_avg = np.mean(data[:, 4])   # 477.3ms
 
 
 #%%
@@ -44,8 +53,32 @@ mrt_avg = ...   # 477.3ms
 # if statement. (i.e., loop through the data to make a sum for each condition, 
 # then divide by the number of data points going into the sum)
 #
-...
+word_count = 0
+face_count = 0
+word_acc_sum = 0
+word_mrt_sum = 0
+face_acc_sum = 0
+face_mrt_sum = 0
 
+for i in range(len(data)): 
+    if data[i, 1] == 1:
+        word_count = word_count + 1
+        word_acc_sum = word_acc_sum + data[i,3]
+        word_mrt_sum = word_mrt_sum + data[i,4]
+    elif data[i, 1] == 2:
+        face_count = face_count + 1
+        face_acc_sum = face_acc_sum + data[i,3]
+        face_mrt_sum = face_mrt_sum + data[i,4]
+    else:
+        break
+
+word_acc_avg = word_acc_sum/word_count
+word_mrt_avg = word_mrt_sum/word_count
+face_acc_avg = face_acc_sum/face_count
+face_mrt_avg = face_mrt_sum/word_count
+
+print(word_acc_avg, word_mrt_avg)
+print(face_acc_avg, face_mrt_avg)
 # words: 88.6%, 489.4ms   faces: 94.4%, 465.3ms
 
 
@@ -55,10 +88,10 @@ mrt_avg = ...   # 477.3ms
 # wp - white/pleasant, bp - black/pleasant
 # (hint: only one line of code is needed per average)
 #
-acc_wp = ...  # 94.0%
-acc_bp = ...  # 88.9%
-mrt_wp = ...  # 469.6ms
-mrt_bp = ...  # 485.1ms
+acc_wp = np.mean(data[(data[:,2] == 1), 3])  # 94.0%
+acc_bp = np.mean(data[(data[:,2] == 2), 3])  # 88.9%
+mrt_wp = np.mean(data[(data[:,2] == 1), 4])  # 469.6ms
+mrt_bp = np.mean(data[(data[:,2] == 2), 4])  # 485.1ms
 
 
 #%% 
@@ -66,7 +99,12 @@ mrt_bp = ...  # 485.1ms
 # use for loops, indexing/slicing, or both!
 # (hint: might be easier to slice data into separate words and faces datasets)
 #
-...
+words_wp_mrt = np.mean(data[(data[:,1] == 1) & (data[:,2] == 1), 4])
+words_bp_mrt = np.mean(data[(data[:,1] == 1) & (data[:,2] == 2), 4])
+faces_wp_mrt = np.mean(data[(data[:,1] == 2) & (data[:,2] == 1), 4])
+faces_bp_mrt = np.mean(data[(data[:,1] == 2) & (data[:,2] == 2), 4])
+
+print(words_wp_mrt, words_bp_mrt, faces_wp_mrt, faces_bp_mrt)
 
 # words - white/pleasant: 478.4ms
 # words - black/pleasant: 500.3ms
@@ -79,7 +117,16 @@ mrt_bp = ...  # 485.1ms
 # paired-sample t-test: scipy.stats.ttest_rel()
 #
 import scipy.stats
-...
+
+words_wp_mrt2 = data[(data[:,1] == 1) & (data[:,2] == 1), 4]
+words_bp_mrt2 = data[(data[:,1] == 1) & (data[:,2] == 2), 4]
+words_t = scipy.stats.ttest_rel(words_wp_mrt2, words_bp_mrt2)
+
+faces_wp_mrt2 = data[(data[:,1] == 2) & (data[:,2] == 1), 4]
+faces_bp_mrt2 = data[(data[:,1] == 2) & (data[:,2] == 2), 4]
+faces_t = scipy.stats.ttest_rel(faces_wp_mrt2, faces_bp_mrt2)
+
+print(words_t, faces_t)
 
 # words: t=-5.36, p=2.19e-5
 # faces: t=-2.84, p=0.0096
@@ -90,4 +137,14 @@ import scipy.stats
 # (hint: use the ''.format() method to create formatted strings)
 #
 print('\nOVERALL: {:.2f}%, {:.1f} ms'.format(100*acc_avg,mrt_avg))
+print('\nWORDS: {:.2f}%, {:.1f} ms'.format(100*word_acc_avg,word_mrt_avg))
+print('\nFACES: {:.2f}%, {:.1f} ms'.format(100*face_acc_avg,face_mrt_avg))
+print('\nWHITE/PLEASANT: {:.2f}%, {:.1f} ms'.format(100*acc_wp,mrt_wp))
+print('\nBLACK/PLEASANT: {:.2f}%, {:.1f} ms'.format(100*acc_bp,mrt_bp))
+print('\nWORDS - WHITE/PLEASANT: {:.1f} ms'.format(words_wp_mrt))
+print('\nWORDS - BLACK/PLEASANT: {:.1f} ms'.format(words_bp_mrt))
+print('\nFACES - WHITE/PLEASANT: {:.1f} ms'.format(faces_wp_mrt))
+print('\nFACES - BLACK/PLEASANT: {:.1f} ms'.format(faces_bp_mrt))
+print('\nWORDS T-TEST: t = {:.2f}, p = {:.4f}'.format(words_t[0], words_t[1]))
+print('\nFACES T-TEST: t = {:.2f}, p = {:.4f}'.format(faces_t[0], faces_t[1]))
 ...
